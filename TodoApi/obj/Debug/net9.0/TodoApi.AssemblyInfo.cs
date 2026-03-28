11706df2bfc9c1db597a50d501df09df63d7abda
Rethabile Siase <ericksiase375@gmail.com>
2026-03-28 12:58:54 +0200

    Add Model TodoItem model and context

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f971c021e3e9b2b5009d3ffabcccf23985e6eb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
